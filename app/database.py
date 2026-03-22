bfabce65b0a91fd52217da3727fcac90b49f7b1a
mikajoos-cmyk <mika.joos@joos-soft-solutions.de>
2026-03-22 10:07:59 +0100

    anderer port

--- app/database.py
+++ app/database.py
@@ -4,12 +4,19 @@ from sqlalchemy.orm import sessionmaker
 from .config import settings
 
 engine = create_engine(
-    settings.DATABASE_URL, # Hier steht deine Pooler-URL mit Port 6543
+    settings.DATABASE_URL, # Hier jetzt die DIRECT URL mit Port 5432 eintragen!
     pool_size=10,
     max_overflow=20,
     pool_timeout=30,
     pool_recycle=1800,
-    pool_pre_ping=True, # Wichtig für den Pooler!
+    pool_pre_ping=True,
+    # TCP Keepalives hinzufügen (nur für psycopg2 relevant)
+    connect_args={
+        "keepalives": 1,
+        "keepalives_idle": 30,
+        "keepalives_interval": 10,
+        "keepalives_count": 5,
+    }
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
